81ed75f644a4269f442df4c310295f0decbb83c6
poshelwon <poshelwon@yandex.ru>
2022-07-11 22:51:37 +0700

    Edit class Main

--- Range/src/ru/javacourse/ryabushkin/range_main/Main.java
+++ Range/src/ru/javacourse/ryabushkin/range_main/Main.java
@@ -2,6 +2,6 @@ package ru.javacourse.ryabushkin.range_main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world..");
+        System.out.println("Hello world...");
     }
 }
