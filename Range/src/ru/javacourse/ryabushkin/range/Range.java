69f9fd4ac2fc45ff82b5442c51dff500756fa71a
poshelwon <poshelwon@yandex.ru>
2022-07-12 21:24:52 +0700

    Task number one

--- Range/src/ru/javacourse/ryabushkin/range/Range.java
+++ Range/src/ru/javacourse/ryabushkin/range/Range.java
@@ -1,4 +1,36 @@
 package ru.javacourse.ryabushkin.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom(double from) {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+
+    public double getTo(double to) {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return from <= number && number <= to;
+    }
 }
--- Range/src/ru/javacourse/ryabushkin/range_main/Main.java
+++ Range/src/ru/javacourse/ryabushkin/range_main/Main.java
@@ -1,7 +1,36 @@
 package ru.javacourse.ryabushkin.range_main;
 
+import ru.javacourse.ryabushkin.range.Range;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world...");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите начальное значение диапозона: ");
+        double from = scanner.nextDouble();
+
+        System.out.print("Введите конечное значение диапозона: ");
+        double to = scanner.nextDouble();
+
+        Range range = new Range(from, to);
+
+        from = range.getFrom(from) - 1;
+        to = range.getTo(to) - 1;
+
+        range.setFrom(from);
+        range.setTo(to);
+
+        System.out.println("Длина обработанного диапазона чисел = " + range.getLength());
+
+        System.out.print("Введите число, которое следует проверить на принадлежность диапазону: ");
+        double number = scanner.nextDouble();
+
+        if (range.isInside(number)) {
+            System.out.println("Число принадлежит к заданному диапазону.");
+        } else {
+            System.out.println("Число не принадлежит к заданному диапазону.");
+        }
     }
 }
