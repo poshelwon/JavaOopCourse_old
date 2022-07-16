f19f2c96a7bba1db42c53665619662f963fef81c
poshelwon <poshelwon@yandex.ru>
2022-07-16 13:31:02 +0700

    Task number two

--- Range/src/ru/javacourse/ryabushkin/range/Range.java
+++ Range/src/ru/javacourse/ryabushkin/range/Range.java
@@ -6,6 +6,7 @@ public class Range {
 
     public Range() {
     }
+
     public Range(double from, double to) {
         this.from = from;
         this.to = to;
@@ -37,15 +38,113 @@ public class Range {
     }
 
     public Range getIntersection(Range range) {
-        if (this.to < range.getFrom() || range.getTo() < this.from) {
+        if (this.to <= range.getFrom() || this.from >= range.getTo()) {
             return null;
         }
 
-        Range intersectionRange = new Range();
+        Range rangesIntersection = new Range();
 
-        intersectionRange.setFrom(Math.max(this.from, range.getFrom()));
-        intersectionRange.setTo(Math.min(this.to, range.getTo()));
+        rangesIntersection.setFrom(Math.max(this.from, range.getFrom()));
+        rangesIntersection.setTo(Math.min(this.to, range.getTo()));
 
-        return intersectionRange;
+        return rangesIntersection;
+    }
+
+    public Range[] getUnion(Range range) {
+        if (this.getIntersection(range) == null) {
+            if (this.from == range.getTo() || this.to == range.getFrom()) {
+                Range[] rangesUnion = new Range[1];
+
+                Range tempRange = new Range();
+
+                tempRange.setFrom(Math.min(this.from, range.getFrom()));
+                tempRange.setTo(Math.max(this.to, range.getTo()));
+
+                rangesUnion[0] = tempRange;
+
+                return rangesUnion;
+            }
+
+            Range[] rangesUnion = new Range[2];
+
+            rangesUnion[0] = this;
+            rangesUnion[1] = range;
+
+            return rangesUnion;
+        }
+
+        Range[] rangesUnion = new Range[1];
+
+        Range tempRange = new Range();
+
+        tempRange.setFrom(Math.min(this.from, range.getFrom()));
+        tempRange.setTo(Math.max(this.to, range.getTo()));
+
+        rangesUnion[0] = tempRange;
+
+        return rangesUnion;
+    }
+
+    public Range[] getComplement(Range range) {
+        Range[] rangesComplement;
+
+        if (getIntersection(range) == null) { // 6
+            rangesComplement = new Range[1];
+
+            rangesComplement[0] = this;
+
+            return rangesComplement;
+        }
+
+        if (this.from > range.getFrom() && this.to > range.getTo()) { // 5
+            rangesComplement = new Range[1];
+
+            Range tempRange = new Range();
+
+            tempRange.setFrom(range.getTo());
+            tempRange.setTo(this.to);
+
+            rangesComplement[0] = tempRange;
+
+            return rangesComplement;
+        }
+
+        if (this.from < range.getFrom() && this.to < range.getTo()) { // 4
+            rangesComplement = new Range[1];
+
+            Range tempRange = new Range();
+
+            tempRange.setFrom(this.from);
+            tempRange.setTo(range.getFrom());
+
+            rangesComplement[0] = tempRange;
+
+            return rangesComplement;
+        }
+
+        if (this.from < range.getFrom() && this.to > range.getTo()) {  // 2
+            rangesComplement = new Range[2];
+
+            Range tempRange1 = new Range();
+
+            tempRange1.setFrom(this.from);
+            tempRange1.setTo(range.getFrom());
+
+            rangesComplement[0] = tempRange1;
+
+            Range tempRange2 = new Range();
+
+            tempRange2.setFrom(range.getTo());
+            tempRange2.setTo(this.to);
+
+            rangesComplement[1] = tempRange2;
+
+            return rangesComplement;
+        }
+
+       /* if (this.from >= range.getFrom() && this.to <= range.getTo()) { // 1, 3
+            return null;
+        } */
+        return null;
     }
 }
--- Range/src/ru/javacourse/ryabushkin/range_main/Main.java
+++ Range/src/ru/javacourse/ryabushkin/range_main/Main.java
@@ -5,30 +5,29 @@ import ru.javacourse.ryabushkin.range.Range;
 public class Main {
     public static void main(String[] args) {
         double oneRangeBegin = 0;
-        double oneRangeEnd = 5.15;
+        double oneRangeEnd = 4;
 
         Range range = new Range(oneRangeBegin, oneRangeEnd);
 
         range.setFrom(oneRangeBegin);
         range.setTo(oneRangeEnd);
 
-        oneRangeBegin = range.getFrom() - 0.01;
-        oneRangeEnd = range.getTo() - 0.01;
+        oneRangeBegin = range.getFrom() - 1;
+        oneRangeEnd = range.getTo() - 1;
 
         range.setFrom(oneRangeBegin);
         range.setTo(oneRangeEnd);
 
-
-        double twoRangeBegin = 5.12;
-        double twoRangeEnd = 7.1;
+        double twoRangeBegin = 4;
+        double twoRangeEnd = 5;
 
         Range range2 = new Range(twoRangeBegin, twoRangeEnd);
 
         range2.setFrom(twoRangeBegin);
         range2.setTo(twoRangeEnd);
-        
-        twoRangeBegin = range2.getFrom() + 0.01;
-        twoRangeEnd = range2.getTo() + 0.01;
+
+        twoRangeBegin = range2.getFrom() + 1;
+        twoRangeEnd = range2.getTo() + 1;
 
         range2.setFrom(twoRangeBegin);
         range2.setTo(twoRangeEnd);
@@ -36,13 +35,44 @@ public class Main {
         double number = 5.14;
 
         if (range.isInside(number) && range2.isInside(number)) {
-            System.out.println("Число принадлежит каждому диапазону.");
+            System.out.println("Число " + number + " принадлежит каждому интервалу.");
         } else  if (range.isInside(number)) {
-            System.out.println("Число принадлежит первому диапазону.");
+            System.out.println("Число " + number + " принадлежит первому интервалу.");
         } else if (range2.isInside(number)) {
-            System.out.println("Число принадлежит второму диапазону.");
+            System.out.println("Число " + number + " принадлежит второму интервалу.");
         } else {
-            System.out.println("Число не принадлежит диапазонам.");
+            System.out.println("Число " + number + " не принадлежит интервалам.");
+        }
+
+        System.out.println("Длина первого интервала = " + range.getLength());
+
+        Range rangesIntersection = range.getIntersection(range2);
+
+        if (rangesIntersection == null) {
+            System.out.println("Интервалы не пересекаются.");
+        } else {
+            System.out.println("Интервалы пересекаются в диапазоне от " + rangesIntersection.getFrom()
+                    + " до " + rangesIntersection.getTo());
+        }
+
+        Range[] rangesUnion = range.getUnion(range2);
+
+        System.out.print("Объединение двух интервалов раполагается в диапозоне");
+        for (Range ranges : rangesUnion) {
+            System.out.print(" от " + ranges.getFrom() + " до " + ranges.getTo());
+        }
+        System.out.println();
+
+        Range[] rangesComplement = range.getComplement(range2);
+
+        if (rangesComplement == null) {
+            System.out.println("Разность интервалов равна пустому множеству");
+        } else {
+            System.out.print("Разность двух интервалов раполагается в диапозоне");
+            for (Range ranges : rangesComplement) {
+                System.out.print(" от " + ranges.getFrom()
+                        + " до " + ranges.getTo());
+            }
         }
     }
 }
